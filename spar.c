e3f709490c38409ff93032a955749e54ede3c43a
B0RJA <borja.latorre@eead.csic.es>
2013-09-10 13:22:19 +0200

    Include headers

--- spar.c
+++ spar.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Arbitrary data type
 #define sparType int
 
--- sparChar.c
+++ sparChar.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Do not edit! Automatically-generated file
 
 // Matrix struct
